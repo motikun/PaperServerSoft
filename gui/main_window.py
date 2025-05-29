95238808b379344181a5410cf946141cb1ad9406
motikun <maikura1982@outlook.com>
2025-05-29 22:43:26 +0900

    v1.1.9
    作成したサーバーをディレクトリとして一覧で見れるようにしました

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QMessageBox
 from PyQt6.QtCore import Qt
 from create import CreateServerDialog
 import sys
@@ -9,6 +9,11 @@ def launch_app():
     layout = QVBoxLayout()
     parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     json_path = os.path.join(parent_dir, 'servers.json')
+
+    if not os.path.exists(json_path):
+        with open(json_path, "w", encoding="utf-8") as f:
+            json.dump([], f, indent=4, ensure_ascii=False)
+            
     json_open = open(json_path, 'r', encoding='utf-8')
     json_load = json.load(json_open)
 
@@ -21,7 +26,7 @@ def launch_app():
     button.clicked.connect(lambda: CreateServerDialog().exec())
 
     for item in json_load:
-        label = QLabel(f"{item["dir"]}")
+        label = QLabel(f"{item['dir']}")
         layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignCenter)
 
     layout.addStretch()
