a4aca22c7740d7a730a39ca696dfd62c80aafd95
okakatsuo <shengtianzongyou@gmail.com>
2025-05-29 22:23:49 +0900

    1.1.8
    色々しました

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,9 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 from PyQt6.QtCore import Qt
 from create import CreateServerDialog
 import sys
+import json
+import os
 
 def launch_app():
+    layout = QVBoxLayout()
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    json_path = os.path.join(parent_dir, 'servers.json')
+    json_open = open(json_path, 'r', encoding='utf-8')
+    json_load = json.load(json_open)
+
     app = QApplication(sys.argv)
     window = QWidget()
     window.setWindowTitle("ランチャー")
@@ -12,7 +20,10 @@ def launch_app():
     button.setFixedSize(300, 100)
     button.clicked.connect(lambda: CreateServerDialog().exec())
 
-    layout = QVBoxLayout()
+    for item in json_load:
+        label = QLabel(f"{item["dir"]}")
+        layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignCenter)
+
     layout.addStretch()
     layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignCenter)
     layout.addStretch()
